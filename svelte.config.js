7c597250b176ffb5933342b0be8cf75f0afaeba6
elmaester <olegmorket@gmail.com>
2025-03-06 11:39:52 +0700

    formatting tweaks, deploy settings

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,15 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
+
+const dev = process.env.NODE_ENV === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: dev ? "" : "/beertab"
+    }
+  }
 };
 
 export default config;
